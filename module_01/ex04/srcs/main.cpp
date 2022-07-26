6ad836ba21c3470dcaa038f12c3b3bf91313c39f
Nicolas_XPS <nicolasburat1607@gmail.com>
2022-07-26 16:48:09 +0200

    WIP ex04

--- module_01/ex04/srcs/main.cpp
+++ module_01/ex04/srcs/main.cpp
@@ -1,13 +1,60 @@
 #include "sed_is_for_losers.hpp"
 
+int	ft_strcmp(std::string s1, std::string s2)
+{
+	int nbchartocompare = 0;
+	int i = 0;
+
+	if(s1.length() > s2.length())
+		nbchartocompare = s2.length();
+	else
+		nbchartocompare = s1.length();
+	while(i < nbchartocompare)
+	{
+		if (s1.c_str()[i] == s2.c_str()[i])
+			i++;
+		else
+			break;
+	}
+	if (i == nbchartocompare)
+		return (0);
+	else
+		return (-1);
+}
+
+std::string replace(std::string line, std::string s1, std::string s2)
+{
+	std::string replaced;
+	int i = 0;
+
+	while (line.c_str()[i])
+	{
+		if (line.c_str()[i] == s1.c_str()[0] && !ft_strcmp(&line.c_str()[i], s1))
+		{	
+			replaced += s2;
+			i += s1.length();
+		}
+		else
+		{
+			replaced.push_back(line.c_str()[i]);
+			i++;
+		}
+	}
+	return (replaced);
+}
+
+
+
 int main(int ac, char **av)
 {	
 	std::ifstream infile;
 	std::ofstream outfile;
 	std::string line;
+	std::string s1;
+	std::string s2;
 
 
-	if(ac == 2)
+	if(ac == 4)
 	{
 		infile.open(av[1]);
 		if(!infile)
@@ -16,12 +63,15 @@ int main(int ac, char **av)
 			return (-1);
 		}
 		else
-		{
+		{	
+			s1 = av[2];
+			s2 = av[3];
+			outfile.open("output");
 			std::cout << "Successfully openned the file" << std::endl;
 			while (infile.good())
 			{
 				getline(infile, line);
-				std::cout << line << std::endl;
+				outfile << replace(line, s1, s2) << std::endl;
 			}
 			std::cout << "Reading finished" << std::endl;
 		}
