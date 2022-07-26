e14cce437ced7495621a66c01224858570dd37a5
Nicolas_XPS <nicolasburat1607@gmail.com>
2022-07-26 15:50:05 +0200

    WIP ex04

--- module_01/ex04/srcs/main.cpp
+++ module_01/ex04/srcs/main.cpp
@@ -1,38 +1,31 @@
 #include "sed_is_for_losers.hpp"
 
 int main(int ac, char **av)
-{
-	std::ifstream in_file;
-	std::ifstream out_file;
-	std::string cpyname;
+{	
+	std::ifstream infile;
+	std::ofstream outfile;
 	std::string line;
-	// std::string s1;
-	// std::string s2;
-	
+
+
 	if(ac == 2)
 	{
-		cpyname = av[1] + ".replace";
-		in_file.open(av[1]);
-		if(!in_file)
+		infile.open(av[1]);
+		if(!infile)
 		{
-			std::cout << "No such file found" << std::endl;
+			std::cout << "Error : cannot open the file" << std::endl;
 			return (-1);
 		}
-		out_file.open(cpyname);
-		//s1 = av[2];
-		//s2 = av[3];
-		if(in_file && out_file)
+		else
 		{
-			while (std::getline(in_file, line))
+			std::cout << "Successfully openned the file" << std::endl;
+			while (infile.good())
 			{
+				getline(infile, line);
 				std::cout << line << std::endl;
 			}
 			std::cout << "Reading finished" << std::endl;
 		}
-		else
-			std::cout << "Cannot read from files" << std::endl;
-		in_file.close();
-		out_file.close();
+		infile.close();
 	}
 	return (0);
 }
