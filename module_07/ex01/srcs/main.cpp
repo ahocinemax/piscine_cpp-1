a329f0eb1b39eae261f2c82840b27ea43cd81043
Nicolas_XPS <nicolasburat1607@gmail.com>
2022-08-18 14:29:42 +0200

    DONE module_07 ex01

--- module_07/ex01/srcs/main.cpp
+++ module_07/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 13:52:39 by nburat-d          #+#    #+#             */
-/*   Updated: 2022/08/18 13:59:15 by nburat-d         ###   ########.fr       */
+/*   Updated: 2022/08/18 14:29:31 by nburat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,12 @@ int	addOne(int nbr)
 	return(nbr);
 }
 
+char	upperCase(char c)
+{
+	c -= 32;
+	return(c);
+}
+
 int main()
 {
 	int nbr[4] = {0, 1, 2, 3};
@@ -26,6 +32,14 @@ int main()
 	for(int i = 0; i < 4; i++)
 	{
 		std::cout << nbr[i] << std::endl;
+	}
+
+	char chartest[4] = {'a', 'b', 'c', 'd'};
+
+	iter<char>(chartest, 4, upperCase);
+	for(int i = 0; i < 4; i++)
+	{
+		std::cout << chartest[i] << std::endl;
 	}
 	
 	return (0);
