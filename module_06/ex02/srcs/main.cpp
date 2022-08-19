e7a7d0506fb11a81fabba4036dd5d243cab9350e
Nicolas_XPS <nicolasburat1607@gmail.com>
2022-08-19 15:36:23 +0200

    FIX module_06 ex02 try n catch with references

--- module_06/ex02/srcs/main.cpp
+++ module_06/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 15:56:59 by nburat-d          #+#    #+#             */
-/*   Updated: 2022/08/17 16:38:23 by nburat-d         ###   ########.fr       */
+/*   Updated: 2022/08/19 15:34:27 by nburat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include "C.hpp"
 #include <stdlib.h>
 
-Base * generate(void)
+Base *generate(void)
 {
 	time_t t;
 	
@@ -61,24 +61,45 @@ void identify(Base* p)
 
 void identify(Base& p)
 {
-	if (dynamic_cast<A*>(&p))
+	try
 	{
-		std::cout << "It s an A class Object" << std::endl;
+		A & ref = dynamic_cast<A&>(p);
+		(void) ref;
+		std::cout << "It s an A class Object" << std::endl;	
+		return ;
 	}
-	if (dynamic_cast<B*>(&p))
+	catch(const std::exception& e)
 	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		B & ref = dynamic_cast<B&>(p);
+		(void) ref;
 		std::cout << "It s an B class Object" << std::endl;
+		return ;
 	}
-	if (dynamic_cast<C*>(&p))
+	catch(const std::exception& e)
 	{
-		std::cout << "It s an C class Object" << std::endl;
+		std::cerr << e.what() << '\n';
 	}
-	
+	try
+	{
+		C & ref = dynamic_cast<C&>(p);
+		(void) ref;
+		std::cout << "It s an C class Object" << std::endl;
+		return ;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
 
 int main()
 {
 	Base* ptrbase = generate();
+	
 	
 	identify(ptrbase);
 	identify(*ptrbase);
