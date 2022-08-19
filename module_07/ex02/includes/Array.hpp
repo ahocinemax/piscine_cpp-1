e11a3138a2746051f17ed27eabd4cd6d2bbd655f
Nicolas_XPS <nicolasburat1607@gmail.com>
2022-08-19 13:49:29 +0200

    WIP module_07 ex02

---
+++ module_07/ex02/includes/Array.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/19 09:58:30 by nburat-d          #+#    #+#             */
+/*   Updated: 2022/08/19 13:10:47 by nburat-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+# include <exception>
+# include <iostream>
+
+
+template <typename T>
+class Array
+{
+	private :
+		T *_array;
+		int	_size;
+
+	public :
+		Array(); 
+		Array(unsigned int n);
+		Array(const Array &src); // Construction by copy and assignment operator. In both cases, modifying either the original array or its copy after copying musn’t affect the other array
+		Array &operator=(const Array &rhs); // You MUST use the operator new[] to allocate memory
+		T &operator[](unsigned int i); // When accessing an element with the [ ] operator, if its index is out of bounds, an std::exception is thrown.
+		
+		int& size() const; 
+
+		class OUT_OF_BOUND : virtual public std::exception
+		{
+			const char* what() {return("Error : out of bound");}
+		};
+};
+
+#endif
--- module_07/ex02/srcs/Array.cpp
+++ module_07/ex02/srcs/Array.cpp
@@ -1,12 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Array.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/19 09:49:00 by nburat-d          #+#    #+#             */
-/*   Updated: 2022/08/19 09:49:01 by nburat-d         ###   ########.fr       */
+/*   Created: 2022/08/19 12:52:45 by nburat-d          #+#    #+#             */
+/*   Updated: 2022/08/19 13:37:01 by nburat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
+template <typename T> Array<T>::Array() // create an empty array
+{
+	T *_array = new T[0];
+	_array[0] = 0;
+}
+
+
+template <typename T> Array<T>::Array(unsigned int n) // create array of n elements initialized by default
+{
+	T *_array = new T[n];
+	for(int i = 0; i < n; i++)
+	{
+		_array[i] = 0
+template <typename T> Array<T>::Array(const Array &src)
+{
+	
+}
